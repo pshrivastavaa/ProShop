ea6709954bb758deb8f297ef44a0c377ffd9d35a
Parth Shrivastava <parth.shrivastava11@gmail.com>
2020-11-06 23:58:01 +0530

    Completed cart page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,20 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 
 const reduces = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 })
-const initialState = {}
+const cartItemsFromStroage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromStroage
+    }
+}
 const middelware = [thunk]
 
 const store = createStore(
