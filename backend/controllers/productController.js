289ace656f2c48318eec2f2721eed3fdaef38fb0
Parth Shrivastava <parth.shrivastava11@gmail.com>
2020-11-07 23:00:04 +0530

    user auth backend completed

---
+++ backend/controllers/productController.js
@@ -0,0 +1,33 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+
+// @desc    Fetch All prodects
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+})
+
+// ======================================================
+
+// @desc    Fetch single prodects
+// @route   GET /api/products/:id
+// @access  Public
+const getProductsById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        res.json(product)
+    } else {
+        // res.status(404).json({ message: 'Product not found' })  //if no middleware
+        res.status(404)                                           //if i have middleware
+        throw new Error('Product not found')                     //if i have middleware
+    }
+})
+
+export {
+    getProducts,
+    getProductsById
+}
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,32 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
 const router = express.Router()
-import Product from '../models/productModel.js'
-
-// @desc    Fetch All prodects
-// @route   GET /api/products
-// @access  Public
-router.get('/', asyncHandler(async (req, res) => {
-
-    const products = await Product.find({})
-    res.json(products)
-}))
+import { getProducts, getProductsById } from '../controllers/productController.js'
 
 
-// @desc    Fetch single prodects
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
 
-    if (product) {
-        res.json(product)
-    } else {
-        // res.status(404).json({ message: 'Product not found' })  //if no middleware
-        res.status(404)                                           //if i have middleware
-        throw new Error('Product not found')                     //if i have middleware
-    }
-
-}))
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductsById)
 
 export default router
